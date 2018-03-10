7d9cde1c884bcd6899ceef9bb3d261e3c6bed728
Serhii Kononov <sergei97k@gmail.com>
2018-03-10 16:27:51 +0200

    added all fields from page

--- main.py
+++ main.py
@@ -48,31 +48,49 @@ for filename in lst:
         main = item.find('a', {'class': 'player_name_players_table'}).text
         info = item.find('span', {'class': 'players_club_nation'})
         main_img = item.find('img', {'class': 'player_img'})['src']
+        person_info = item.find('a', {'class': 'player_name_players_table'})['href']
+
         version_info = item.find('span', {'class': 'rating'})['class'][2:-1]
         version_info.reverse()
 
+        club_info = info.select('a')[0]
+        nation_info = info.select('a')[1]
+        league_info = info.select('a')[2]
+
+        count_games = int(item.find('td', {'class': 'num_td'}).text.replace(',', ''))
+
         tmp_dict = {
+            'player_id': int(re.findall('\d+', person_info)[1]),
             'name': main[0 : main.find(' (')],
             'position': main[main.find('(') + 1 : main.find(' )')],
+            'rating': int(item.find('span', {'class': 'rating'}).text),
+            'person_page': person_info,
+            'img': main_img,
+            'games': count_games,
             'club': {
-                'name': info.select('a')[0]['data-original-title'],
-                'img': info.select('a')[0].find('img')['src']
+                'id': int(re.findall('\d+', club_info['href'])[2]),
+                'name': club_info['data-original-title'],
+                'img': club_info.find('img')['src']
             },
             'nation': {
-                'name': info.select('a')[1]['data-original-title'],
-                'img': info.select('a')[1].find('img')['src']
+                'id': int(re.findall('\d+', nation_info['href'])[2]),
+                'name': nation_info['data-original-title'],
+                'img': nation_info.find('img')['src']
             },
             'league': {
-                'name': info.select('a')[2]['data-original-title'],
-                'img': info.select('a')[2].find('img')['src']
+                'id': int(re.findall('\d+', league_info['href'])[2]),
+                'name': league_info['data-original-title'],
+                'img': league_info.find('img')['src']
             },
-            'img': main_img,
             'version': {
                 'quality': version_info[1],
                 'type': version_info[0],
                 'special': version_info[len(version_info) - 1] if len(version_info) == 3 else 'null'
             },
-            'rating': int(item.find('span', {'class': 'rating'}).text)
+            'price': {
+                'ps4': item.find('span', {'class': 'ps4_color'}).text,
+                'xb1': item.find('span', {'class': 'xb1_color'}).text
+            }
         }
 
         data_table.append(tmp_dict)
